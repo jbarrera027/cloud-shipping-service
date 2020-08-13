fba15a41b789dd03105e018f2959070852b824a8
jbarrera027 <67080955+jbarrera027@users.noreply.github.com>
2020-08-13 14:03:15 -0500

    Add files via upload

---
+++ src/main/java/co/edu/cedesistemas/commerce/shipping/CloudShippingServiceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.cedesistemas.commerce.shipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudShippingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudShippingServiceApplication.class, args);
+	}
+
+}
